8b717ee8f6cd85934d62b4a3499291c7622c77b5
Auxes1s <muignacio@up.edu.ph>
2024-04-18 16:22:36 +0800

    Fixing NEDA RCT Exercise
    The felm function is pretty weird when it is called a function. Thus, a do.call is used so that it can be called in other functions, and a summary(felm) is used so that we can normally output model summary.

--- Session 3.3 RCT/Codes/NEDA RCT Exercise.R
+++ Session 3.3 RCT/Codes/NEDA RCT Exercise.R
@@ -1,5 +1,5 @@
 # Install packages
-packages <- c("tidyverse", "broom", "dplyr", "haven", "lmtest", "lfe");for(pack in packages){
+packages <- c("tidyverse", "dplyr", "haven", "lfe");for(pack in packages){
   if(!require(pack, character.only = TRUE)){
     install.packages(pack, dependencies = TRUE)
     require(pack, character.only = TRUE)
@@ -16,36 +16,34 @@ balance_vars <- c("age", "hhmemnum", "educ", "lit", "hunger6m")
 outcome_vars <- c("life_sat", "future_life_sat", "hhincome30", "lh_asset", "loans")
 
 # Function to perform detailed summary and regression
-areg <- function(vars, data) {
-  results <- list()
-  
-  for (var in vars) {
-    cat("\nAnalysis Results for:", var, "\n")
+areg <- function(vars, df){
+  for(i in vars){
+    cat("\n Analysis for: ", i, "\n")
+    print(summary(df[[i]]))
     
-    # Summary statistics
-    print(summary(data[[var]]))
+    #NOTE: Since FE_LM is weird inside functions, we redundantly called it for the model summary
+    formula_for_fn <- as.formula(paste(i, '~ treatment | community'))
+    fe_lm <- do.call("felm", list(formula=formula_for_fn,
+                                  data=df))
+    print(summary(felm(formula_for_fn, data = df)))
+
+    icpt <- getfe(obj = fe_lm, se = TRUE, robust = TRUE,  ef = 'zm2', bN = 5000)
     
-    # Absorption regression including intercept
-    model <- felm(as.formula(paste(var, "~ treatment | community")), data = data)
-    print(summary(model))
-    
-    # Extract the intercept
-    icpt <- getfe(model, ef='zm2', robust = TRUE)
-    icpt$t_value <- icpt$effect/icpt$se
-    icpt$p_value <- 2 * pt(-abs(icpt$t_value), df = model$df)
-    cons <- c(icpt["icpt.1", c("effect", "se", "t_value", "p_value")])
+    icpt <- icpt %>%
+      mutate(t_value = effect/robustse,
+             p_value = 2 * pt(-abs(t_value), df = fe_lm$df))
+    cons <- c(icpt["icpt.1", c("effect", "robustse", "t_value", "p_value")])
     
     # Print model summary with constant
-    summary_model <- rbind(summary(model)$coefficients,cons)
+    summary_model <- rbind(summary(fe_lm)$coefficients,cons)
     cat("\n Computing for the constant:\n")
     print(summary_model)
-    
-    results[[var]] <- list(model_summary = tidy(model), with_cons = summary_model)
   }
-  
-  return(results)
 }
 
 # Execute the analysis for balance and outcome variables
-balance_results <- areg(balance_vars, df)
-outcome_results <- areg(outcome_vars, df)
+#Balance
+areg(balance_vars, df)
+
+#Outcomes
+areg(outcome_vars, df)
